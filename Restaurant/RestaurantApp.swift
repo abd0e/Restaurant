b6911e1e375016f5b81abd7597c7e6c77df7176a
Abdullah Al Mamari <ab620dua@gmail.com>
2024-12-15 07:55:07 +0400

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Abdullah Almamari on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
